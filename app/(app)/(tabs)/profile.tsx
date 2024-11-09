be371fc205c9efe592f27189a9ae77f715416f39
gitJoakim <joakimgit@gmail.com>
2024-11-09 02:44:22 +0100

    yup, progress

--- app/(app)/(tabs)/profile.tsx
+++ app/(app)/(tabs)/profile.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from "react-native";
+import { useAuthSession } from "@/providers/AuthContextProvider";
+import { View, Text, Pressable } from "react-native";
 
 export default function Profile() {
+	const { userNameSession, logOut } = useAuthSession();
+
 	return (
 		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>Profile</Text>
+			<Text>Hello, {userNameSession}!</Text>
+
+			<Pressable
+				style={{
+					backgroundColor: "red",
+					padding: 16,
+					borderRadius: 8,
+					marginTop: 64,
+				}}
+				onPress={() => {
+					logOut();
+				}}
+			>
+				<Text>Log out</Text>
+			</Pressable>
 		</View>
 	);
 }
--- components/UploadArtworkForm.tsx
+++ components/UploadArtworkForm.tsx
@@ -1,39 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
 
 export default function UploadArtworkForm() {
 	const [hashtag, setHashtag] = useState<string>("#");
 	const [hashtagsArray, setHashtagsArray] = useState<string[]>([]);
 
-	// This function filters the input to allow only alphanumeric characters
-	const handleHashtagChange = (input: string) => {
-		// Makes sure inputfield starts with hashtag and only valid input is english alphabet or numbers
-		// ** Not mine, taken from stack overflow **
+	function handleHashtagChange(input: string) {
+		// makes sure inputfield starts with hashtag and only valid input is english alphabet or numbers
+		// ** not mine, taken from stack overflow **
 		const sanitizedInput = input.replace(/[^a-zA-Z0-9]/g, "").replace(/^#/, "");
-
 		setHashtag(`#${sanitizedInput}`);
-	};
+	}
 
-	// Function to remove hashtag
+	// add hashtag to the array
+	function addHashtag() {
+		if (hashtag.trim() && hashtag !== "#") {
+			setHashtagsArray((prev) => [...prev, hashtag]);
+			setHashtag("#");
+		}
+	}
+
+	// remove a hashtag from the array
 	function removeHashtag(index: number) {
 		setHashtagsArray((prev) => prev.filter((_, i) => i !== index));
 	}
 
-	// JSX to render hashtags
-	const displayHashtagsJSX = () => {
+	// render all the hashtags
+	function renderHashtagsJSX() {
 		return hashtagsArray.map((hashtag: string, index) => (
 			<Pressable
 				key={index}
-				style={styles.hashtagStyle}
-				onPress={() => {
-					removeHashtag(index);
-				}}
+				style={styles.hashtag}
+				onPress={() => removeHashtag(index)}
 			>
-				<Text>{hashtag}</Text>
-				<Text> x</Text>
+				<Text style={styles.hashtagTextColor}>{hashtag}</Text>
 			</Pressable>
 		));
-	};
+	}
 
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -47,24 +50,31 @@ export default function UploadArtworkForm() {
 				<Text>Hashtags</Text>
 				<View style={{ flexDirection: "row" }}>
 					<TextInput
-						style={styles.textInput}
+						style={[styles.textInput, { color: "blue" }]}
 						value={hashtag}
 						onChangeText={handleHashtagChange} // Use the filtered input handler
 						autoCapitalize="none"
 					/>
 					<Pressable
-						onPress={() => {
-							if (hashtag.trim() && hashtag !== "#") {
-								setHashtagsArray((prev) => [...prev, hashtag]);
-								setHashtag("#"); // Reset the input field to only '#'
-							}
-						}}
+						onPress={addHashtag}
 						style={{ backgroundColor: "blue", padding: 6 }}
 					>
 						<Text>Add</Text>
 					</Pressable>
 				</View>
-				<View style={{ gap: 12 }}>{displayHashtagsJSX()}</View>
+
+				<Text
+					style={{
+						textAlign: "center",
+						color: "gray",
+						marginVertical: 6,
+						opacity: hashtagsArray.length === 0 ? 0 : 1,
+					}}
+				>
+					Tap a hashtag to remove it
+				</Text>
+
+				<View style={styles.hashtagsContainer}>{renderHashtagsJSX()}</View>
 			</View>
 		</View>
 	);
@@ -78,8 +88,17 @@ const styles = StyleSheet.create({
 		padding: 6,
 		marginBottom: 32,
 	},
-	hashtagStyle: {
-		backgroundColor: "grey",
+	hashtagsContainer: {
+		gap: 12,
 		flexDirection: "row",
+		justifyContent: "space-evenly",
+		alignItems: "center",
+		flexWrap: "wrap",
+	},
+	hashtag: {
+		backgroundColor: "lightgray",
+	},
+	hashtagTextColor: {
+		color: "blue",
 	},
 });
