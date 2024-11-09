7ea3ac561cfa9b2ab2b3adf67f7c95063e54499d
gitJoakim <joakimgit@gmail.com>
2024-11-09 22:53:35 +0100

    started crud for artwork, we got post

---
+++ api/artworkApi.ts
@@ -0,0 +1,34 @@
+import { db, getDownloadUrl } from "@/firebaseConfig";
+import { ArtworkData } from "@/utils/artworkData";
+import { addDoc, collection } from "firebase/firestore";
+import { uploadImageToFirebase } from "./imageApi";
+
+/*
+*************************************************
+
+This is heavily inspired by lecture code
+
+*************************************************
+*/
+
+export const addArtwork = async (artwork: ArtworkData) => {
+	try {
+		const firebaseImage = await uploadImageToFirebase(artwork.imageURL);
+		if (firebaseImage === "ERROR") {
+			return;
+		}
+		const artworkImageDownloadUrl = await getDownloadUrl(firebaseImage);
+		const artworkWithImageData: ArtworkData = {
+			...artwork,
+			imageURL: artworkImageDownloadUrl,
+		};
+
+		const docRef = await addDoc(
+			collection(db, "artworks"),
+			artworkWithImageData
+		);
+		console.log("Document written with ID:", docRef);
+	} catch (error) {
+		console.log("Error adding document:", error);
+	}
+};
---
+++ api/imageApi.ts
@@ -0,0 +1,32 @@
+import { getStorageRef } from "@/firebaseConfig";
+import { uploadBytesResumable } from "firebase/storage";
+
+/*
+*************************************************
+
+THIS IS TAKEN FROM LECTURE CODE
+
+*************************************************
+*/
+
+export const uploadImageToFirebase = async (uri: string) => {
+	const fetchResponse = await fetch(uri);
+	const blob = await fetchResponse.blob();
+
+	const imagePath = uri.split("/").pop()?.split(".")[0] ?? "unnamed_image";
+	console.log("imagepath", imagePath);
+
+	const uploadPath = `images/${imagePath}`;
+
+	const imageRef = getStorageRef(uploadPath);
+
+	try {
+		console.log("pls");
+		await uploadBytesResumable(imageRef, blob);
+		console.log("Uploading image to", uploadPath);
+		return uploadPath;
+	} catch (e) {
+		console.error("error uploading image", e);
+		return "ERROR";
+	}
+};
